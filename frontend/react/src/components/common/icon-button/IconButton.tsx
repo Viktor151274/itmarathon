6de357347288902ab17e297135fc7876523f73eb
Valerya-Shvets <124630505+Valerya-Shvets@users.noreply.github.com>
2025-08-19 17:59:31 +0300

    [FER] EPMRDUAITM-31 Icon Button (#23)
    * [FER] EPMRDUAITM-31: create icon button
    
    * [FER] EPMRDUAITM-31: fix after review
    
    * [FER] EPMRDUAITM-31: fix after review
    
    * [FER] EPMRDUAITM-31: fix after review

---
+++ frontend/react/src/components/common/icon-button/IconButton.tsx
@@ -0,0 +1,23 @@
+import type { IconButtonProps } from "./types.ts";
+import { ICONS_PATH } from "./utils.ts";
+import "./IconButton.scss";
+
+export const IconButton = ({
+  iconName,
+  color = "green",
+  onClick,
+  ...restProps
+}: IconButtonProps) => {
+  return (
+    <button
+      className={`icon-button icon-button--${color}`}
+      onClick={onClick}
+      type="button"
+      {...restProps}
+    >
+      <svg className="icon-button__icon">
+        <use href={`${ICONS_PATH}${iconName}`} />
+      </svg>
+    </button>
+  );
+};
