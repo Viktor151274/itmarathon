fb5a3c795235db224b563959c3bb44a7916be3bc
Yuriy Mamoshin <yuriymamoshin@gmail.com>
2025-08-20 20:56:27 +0300

    [FEA] EPMRDUAITM-45: fixed variable name, logo path, svg display, header placement

--- frontend/angular/src/app/app.constants.ts
+++ frontend/angular/src/app/app.constants.ts
@@ -1,2 +1,2 @@
 export const ICONS_SPRITE_PATH = '/assets/icons/icons-sprite.svg';
-export const LOGO_SPRITE_PATH = '/assets/icons/logo-sprite.svg';
+export const LOGO_SPRITE_PATH = '/assets/icons/logo-sprite.svg#logo';
