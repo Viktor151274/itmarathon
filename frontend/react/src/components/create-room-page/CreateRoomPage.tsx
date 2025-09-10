eaf44fa72bbd0d87d33f5e789187158f6cc8f226
Valerya-Shvets <124630505+Valerya-Shvets@users.noreply.github.com>
2025-09-10 21:42:00 +0300

    [FER] EPMRDUAITM-102: add general background to variable, add Create room page title (#104)
    * [FER] EPMRDUAITM-102: add general background to _variables.scss
    
    * [FER] EPMRDUAITM-102: fix path to sprite file for general background
    
    * [FER] EPMRDUAITM-102: add general background to %main-elemen
    
    * [FER] EPMRDUAITM-102: rename sprite and apply %main-element styles to the page
    
    ---------
    
    Co-authored-by: Lada Kozbina <lkozbina@gmail.com>

--- frontend/react/src/components/create-room-page/CreateRoomPage.tsx
+++ frontend/react/src/components/create-room-page/CreateRoomPage.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import { CREATE_ROOM_PAGE_TITLE } from "./utils";
+import "./CreateRoomPage.scss";
+
 const CreateRoomPage = () => {
-  return <header>Create Room Page</header>;
+  useEffect(() => {
+    document.title = CREATE_ROOM_PAGE_TITLE;
+  }, []);
+
+  return <main className="create-room-page">Create Room Page</main>;
 };
 
 export default CreateRoomPage;
