0dce0da3e2bf1003046d80f7cf98f7b17e3bdb07
Kostiantyn Ahafonov <98952932+millcons@users.noreply.github.com>
2025-09-29 10:31:35 +0300

    [Devops] Chore: update bucket (#181)
    * chore: Add tfstate bucket
    
    * chore: Change bucket
    
    ---------
    
    Co-authored-by: Kostiantyn Ahafonov <Kostiantyn_Ahafonov@epam.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = ""
+    bucket       = "terraform-tfstate-jkbskefbjek"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
