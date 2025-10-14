febdfc4f57e6233af79bd2427d5931282c87911e
Valerya-Shvets <124630505+Valerya-Shvets@users.noreply.github.com>
2025-10-14 20:54:05 +0300

    [FER] EPMRDUAITM-220: add RoomDetails to the Room Page (#267)
    * [FER] EPMRDUAITM-220: add RoomDetails to the Room Page
    
    * [FER] EPMRDUAITM-220: change imports order
    
    * [FER] EPMRDUAITM-220: fix after review

--- frontend/react/src/components/room-page/room-page-content/RoomPageContent.tsx
+++ frontend/react/src/components/room-page/room-page-content/RoomPageContent.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useParams } from "react-router";
 import ParticipantsList from "../participants-list/ParticipantsList";
+import RoomDetails from "../room-details/RoomDetails";
 import RandomizationModal from "@components/common/modals/randomization-modal/RandomizationModal";
 import RandomizationPanel from "../randomization-panel/RandomizationPanel";
+import { generateInvitationNoteContent } from "@utils/general";
 import { getCurrentUser, getParticipantInfoById } from "./utils";
 import type { WishlistProps } from "@components/common/wishlist/types";
 import type { RoomPageContentProps } from "./types";
@@ -51,6 +53,17 @@ const RoomPageContent = ({
   return (
     <div className="room-page-content">
       <div className="room-page-content-column">
+        <RoomDetails
+          name={roomDetails.name}
+          description={roomDetails.description}
+          exchangeDate={roomDetails.giftExchangeDate}
+          giftBudget={roomDetails.giftMaximumBudget}
+          invitationNote={generateInvitationNoteContent(
+            roomDetails.invitationNote,
+            roomDetails.invitationCode,
+          )}
+          withoutInvitationCard={!isAdmin || isRandomized}
+        />
         <ParticipantsList participants={participants} />
       </div>
 
