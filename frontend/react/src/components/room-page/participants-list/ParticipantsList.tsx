aaed9338f1d9d52f42f80339287c0c0e18dd8dc1
TiMur-know <65873801+TiMur-know@users.noreply.github.com>
2025-10-23 12:46:46 +0300

    [FER] EPMRDUAITM-289: Show ParticipantDetailsModal on InfoButton click (#316)
    * [FER] EPMRDUAITM-289: Show ParticipantDetailsModal on InfoButton click
    
    * [FER] EPMRDUAITM-289: add fixes
    
    * [FER] EPMRDUAITM-289: add fixes

--- frontend/react/src/components/room-page/participants-list/ParticipantsList.tsx
+++ frontend/react/src/components/room-page/participants-list/ParticipantsList.tsx
@@ -1,66 +1,93 @@
+import { useState } from "react";
 import { useParams } from "react-router";
 import ParticipantCard from "@components/common/participant-card/ParticipantCard";
+import ParticipantDetailsModal from "@components/common/modals/participant-details-modal/ParticipantDetailsModal";
+import type { Participant } from "@types/api";
 import {
   MAX_PARTICIPANTS_NUMBER,
   generateParticipantLink,
 } from "@utils/general";
-import { type ParticipantsListProps } from "./types";
+import { type ParticipantsListProps, type PersonalInformation } from "./types";
 import "./ParticipantsList.scss";
 
 const ParticipantsList = ({ participants }: ParticipantsListProps) => {
   const { userCode } = useParams();
+  const [selectedParticipant, setSelectedParticipant] =
+    useState<PersonalInformation | null>(null);
+
   const admin = participants?.find((participant) => participant?.isAdmin);
   const restParticipants = participants?.filter(
     (participant) => !participant?.isAdmin,
   );
 
-  const handleInfoButtonClick = () => {};
+  const handleInfoButtonClick = (participant: Participant) => {
+    const personalInfoData: PersonalInformation = {
+      firstName: participant.firstName,
+      lastName: participant.lastName,
+      phone: participant.phone,
+      deliveryInfo: participant.deliveryInfo,
+      email: participant.email,
+      link: generateParticipantLink(participant.userCode),
+    };
+    setSelectedParticipant(personalInfoData);
+  };
+
+  const handleModalClose = () => setSelectedParticipant(null);
 
   return (
-    <div className="participant-list">
-      <div className="participant-list-header">
-        <h3 className="participant-list-header__title">Who’s Playing?</h3>
+    <>
+      <div className="participant-list">
+        <div className="participant-list-header">
+          <h3 className="participant-list-header__title">Who’s Playing?</h3>
 
-        <span className="participant-list-counter__current">
-          {participants?.length ?? 0}/
-        </span>
+          <span className="participant-list-counter__current">
+            {participants?.length ?? 0}/
+          </span>
 
-        <span className="participant-list-counter__max">
-          {MAX_PARTICIPANTS_NUMBER}
-        </span>
+          <span className="participant-list-counter__max">
+            {MAX_PARTICIPANTS_NUMBER}
+          </span>
+        </div>
+
+        <div className="participant-list__cards">
+          {admin ? (
+            <ParticipantCard
+              key={admin?.id}
+              firstName={admin?.firstName}
+              lastName={admin?.lastName}
+              isCurrentUser={userCode === admin?.userCode}
+              isAdmin={admin?.isAdmin}
+              isCurrentUserAdmin={userCode === admin?.userCode}
+              adminInfo={`${admin?.phone}${admin?.email ? `\n${admin?.email}` : ""}`}
+              participantLink={generateParticipantLink(admin?.userCode)}
+            />
+          ) : null}
+
+          {restParticipants?.map((user) => (
+            <ParticipantCard
+              key={user?.id}
+              firstName={user?.firstName}
+              lastName={user?.lastName}
+              isCurrentUser={userCode === user?.userCode}
+              isCurrentUserAdmin={userCode === admin?.userCode}
+              participantLink={generateParticipantLink(user?.userCode)}
+              onInfoButtonClick={
+                userCode === admin?.userCode && userCode !== user?.userCode
+                  ? () => handleInfoButtonClick(user)
+                  : undefined
+              }
+            />
+          ))}
+        </div>
       </div>
-
-      <div className="participant-list__cards">
-        {admin ? (
-          <ParticipantCard
-            key={admin?.id}
-            firstName={admin?.firstName}
-            lastName={admin?.lastName}
-            isCurrentUser={userCode === admin?.userCode}
-            isAdmin={admin?.isAdmin}
-            isCurrentUserAdmin={userCode === admin?.userCode}
-            adminInfo={`${admin?.phone}${admin?.email ? `\n${admin?.email}` : ""}`}
-            participantLink={generateParticipantLink(admin?.userCode)}
-          />
-        ) : null}
-
-        {restParticipants?.map((user) => (
-          <ParticipantCard
-            key={user?.id}
-            firstName={user?.firstName}
-            lastName={user?.lastName}
-            isCurrentUser={userCode === user?.userCode}
-            isCurrentUserAdmin={userCode === admin?.userCode}
-            participantLink={generateParticipantLink(user?.userCode)}
-            onInfoButtonClick={
-              userCode === admin?.userCode && userCode !== user?.userCode
-                ? handleInfoButtonClick
-                : null
-            }
-          />
-        ))}
-      </div>
-    </div>
+      {selectedParticipant ? (
+        <ParticipantDetailsModal
+          isOpen={!!selectedParticipant}
+          onClose={handleModalClose}
+          personalInfoData={selectedParticipant}
+        />
+      ) : null}
+    </>
   );
 };
 
--- frontend/react/src/components/room-page/participants-list/types.ts
+++ frontend/react/src/components/room-page/participants-list/types.ts
@@ -3,3 +3,12 @@ import type { GetParticipantsResponse } from "@types/api.ts";
 export interface ParticipantsListProps {
   participants: GetParticipantsResponse;
 }
+
+export interface PersonalInformation {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email?: string;
+  deliveryInfo: string;
+  link?: string;
+}
